dfce360e88fc28d95712901482bfa9560a8ae54c
daoliveira <daoliveira@visao.com>
2026-02-25 23:36:18 -0300

    Fix port for Fly.io

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const empresaRoutes = require('./routes/empresa');
 const usuariosRoutes = require('./routes/usuarios');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
